4569722a9022fd3b71ec813277c17e812438ffe5
eFrench8 <164631948+eFrench8@users.noreply.github.com>
2024-10-10 13:26:07 +0000

    testing api

--- testingFeatures/gettingRSI.py
+++ testingFeatures/gettingRSI.py
@@ -1,1 +1,9 @@
 #use alpha vantage to get the rsi data and work out if i should buy or sell the stock
+
+import requests
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey='
+r = requests.get(url)
+data = r.json()
+
+print(data)
